b6f5534f92e6e2ae69459cad981c82cc3013a098
Sunny3oy <meisunny1992@hotmail.com>
2017-04-11 14:01:16 -0400

    fixed some issues, gonna retrace what i did for the past few days....

--- Fourguys.py
+++ Fourguys.py
@@ -6,7 +6,6 @@ from wtforms import StringField, PasswordField, BooleanField, IntegerField
 from wtforms.validators import InputRequired, Email, Length
 from flask_sqlalchemy import SQLAlchemy
 
-
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config['SECRET_KEY'] = 'key'
