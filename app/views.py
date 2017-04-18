8dea686be4b504af7e1d784cccbf93df2eec2e94
Ichwan Palongengi <ich9681@gmail.com>
2017-04-18 10:26:35 -0400

    Merge changes from Sunny to the Master Branch
    - Refactor Sunny's code to reflect changes in the
      database model it uses
    - Add *.exe and *.pyc into .gitignore

--- app/views.py
+++ app/views.py
@@ -37,16 +37,16 @@ def menu():
     if form.validate_on_submit():
         doge = 1
         total = [0,0,0]
-        databaseprice1 = Menu.query.filter_by(menuID=1).first()
-        databaseprice2 = Menu.query.filter_by(menuID=2).first()
-        databaseprice3 = Menu.query.filter_by(menuID=3).first()
-        total[0] = databaseprice1.menuPrice * form.qty0.data
-        total[1] = databaseprice2.menuPrice * form.qty1.data
-        total[2] = databaseprice3.menuPrice * form.qty2.data
+        databaseprice1 = FoodItem.query.filter_by(itemID=1).first()
+        databaseprice2 = FoodItem.query.filter_by(itemID=2).first()
+        databaseprice3 = FoodItem.query.filter_by(itemID=3).first()
+        total[0] = databaseprice1.itemPrice * form.qty0.data
+        total[1] = databaseprice2.itemPrice * form.qty1.data
+        total[2] = databaseprice3.itemPrice * form.qty2.data
         sumitem = sum(total)
         print (total[0],total[1],total[2])
-        return render_template("menu.html", form=form, databaseitems=Menu.query.all(), doge=doge,total = total,sumitem = sumitem)
-    return render_template("menu.html", form=form,databaseitems = Menu.query.all(),doge=doge,sumitem = sumitem)
+        return render_template("menu.html", form=form, databaseitems=FoodItem.query.all(), doge=doge,total = total,sumitem = sumitem)
+    return render_template("menu.html", form=form,databaseitems = FoodItem.query.all(), doge=doge,sumitem = sumitem)
 
 @app.route('/checkout')
 def check():
